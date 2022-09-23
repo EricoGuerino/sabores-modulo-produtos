d6611e0554449f405d3220eaa04a9729f7bc4267
Erico Guerino <ericofgg@gmail.com>
2022-09-23 20:22:28 -0300

    Projeto Criado

---
+++ src/main/java/com/tcc/saboresmoduloprodutos/SaboresModuloProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.saboresmoduloprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaboresModuloProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaboresModuloProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/saboresmoduloprodutos/SaboresModuloProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.saboresmoduloprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaboresModuloProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
